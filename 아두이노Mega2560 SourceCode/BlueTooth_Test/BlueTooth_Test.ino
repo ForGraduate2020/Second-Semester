819ff7d23594046d789ec239edd68b836ee74d3b
Livenow14 <xowjd41@naver.com>
2020-02-03 19:33:48 +0900

    BlueTooht Test 완료

--- 아두이노Mega2560 SourceCode/BlueTooth_Test/BlueTooth_Test.ino
+++ 아두이노Mega2560 SourceCode/BlueTooth_Test/BlueTooth_Test.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#define MEGA2560 1              //0: Uno, 1: Mega 2560
 
+#if MEGA2560
+#define mySerial Serial3
+#else
+#include <SoftwareSerial.h>
+int blueTx = 2;
+int blueRx = 3;
+SoftwareSerial mySerial(blueTx, blueRx);
+#endif
+
+#define BAUDRATE 9600              //select 9600 or 115200 w.r.t module
+
+void setup()
+{
+  Serial.begin(9600);
+  mySerial.begin(BAUDRATE);
+
+  pinMode(8, OUTPUT);                   //HC-05,   EN
+  digitalWrite(8, HIGH);
+  Serial.println("ATcommand");          //ATcommand Start
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
 }
